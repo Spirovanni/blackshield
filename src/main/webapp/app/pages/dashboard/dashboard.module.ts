43845a9c0cf511c68db4cbfddbf6fce09c857b53
Xavier Martinez <blackshieldsx@gmail.com>
2019-06-23 14:09:29 -0700

    113. Attach the electricity components

--- src/main/webapp/app/pages/dashboard/dashboard.module.ts
+++ src/main/webapp/app/pages/dashboard/dashboard.module.ts
@@ -4,17 +4,17 @@ import { NgxEchartsModule } from 'ngx-echarts';
 
 import { ThemeModulex } from '../../shared/@theme/theme-modulex.module';
 import { DashboardComponent } from './dashboard.component';
-// import { StatusCardComponent } from './status-card/status-card.component';
+import { StatusCardComponent } from './status-card/status-card.component';
 // import { ContactsComponent } from './contacts/contacts.component';
 // import { RoomsComponent } from './rooms/rooms.component';
 // import { RoomSelectorComponent } from './rooms/room-selector/room-selector.component';
-// import { TemperatureComponent } from './temperature/temperature.component';
-// import { TemperatureDraggerComponent } from './temperature/temperature-dragger/temperature-dragger.component';
+import { TemperatureComponent } from './temperature/temperature.component';
+import { TemperatureDraggerComponent } from './temperature/temperature-dragger/temperature-dragger.component';
 // import { TeamComponent } from './team/team.component';
 // import { KittenComponent } from './kitten/kitten.component';
 // import { SecurityCamerasComponent } from './security-cameras/security-cameras.component';
-// import { ElectricityComponent } from './electricity/electricity.component';
-// import { ElectricityChartComponent } from './electricity/electricity-chart/electricity-chart.component';
+import { ElectricityComponent } from './electricity/electricity.component';
+import { ElectricityChartComponent } from './electricity/electricity-chart/electricity-chart.component';
 // import { WeatherComponent } from './weather/weather.component';
 // import { SolarComponent } from './solar/solar.component';
 // import { PlayerComponent } from './rooms/player/player.component';
@@ -24,18 +24,18 @@ import { DashboardComponent } from './dashboard.component';
 @NgModule({
   imports: [ThemeModulex, NgxEchartsModule],
   declarations: [
-    DashboardComponent
-    // StatusCardComponent,
-    // TemperatureDraggerComponent,
+    DashboardComponent,
+    StatusCardComponent,
+    TemperatureDraggerComponent,
     // ContactsComponent,
     // RoomSelectorComponent,
-    // TemperatureComponent,
+    TemperatureComponent,
     // RoomsComponent,
     // TeamComponent,
     // KittenComponent,
     // SecurityCamerasComponent,
-    // ElectricityComponent,
-    // ElectricityChartComponent,
+    ElectricityComponent,
+    ElectricityChartComponent
     // WeatherComponent,
     // PlayerComponent,
     // SolarComponent,
